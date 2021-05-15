1f5f358ef2aa4d5c59f44262fc6ac87732fee559
Juan Melo Pérez <36827460+juanmlp@users.noreply.github.com>
2021-05-15 20:21:09 +0100

    New version
    Scrapping web using Puppeteer library

--- script.js
+++ script.js
@@ -1,46 +1,88 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+//const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 const url = "https://coinmarketcap.com/";
 
-axios.get(url).then(response => {
+(async () => {
 
-        var $ = cheerio.load(response.data);
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
 
-        const cryptoName = [];
-        const prices = [];
-        const weekPriceChange = [];
-        const marketCap = [];
+    //var $ = cheerio.load(response.data);
 
-        const cryptos = [];
+    const cryptoName = [];
+    const prices = [];
+    const weekPriceChange = [];
+    const marketCap = [];
 
-
-        // Obtener nombres
-        $('.cmc-table tr td .sc-1eb5slv-0.iJjGCS').each(function () {
-            cryptoName.push($(this).html());
-        })
-
-        // Obtener precios
-        $('.cmc-table tr td .price___3rj7O ').each(function () {
-            prices.push($(this).text());
-        })
+    const cryptos = [];
 
 
-        // Obtener precios
-        $('.cmc-table tr td .sc-1v2ivon-0.fJLBDK').each(function () {
-            weekPriceChange.push($(this).text());
-        })
+    await scrollDown(page);
 
 
-        // Obtener Cap. Mercado
-        $('.cmc-table tr td .sc-1eb5slv-0.kDEzev').each(function () {
-            marketCap.push($(this).text());
-        })
+    await page.screenshot({
+        path: 'yoursite.png',
+        fullPage: true
+    });
 
 
-        for (i = 0; i <= cryptoName.length; i++) {
-            cryptos.push({ name: cryptoName[i], price: prices[i], priceChanged: weekPriceChange[i], marketCap: marketCap[i] });
+
+    // Obtener nombres
+
+    this.cryptoName = await page.evaluate(() => {
+        const data = Array.from(document.querySelectorAll('.cmc-table tr td .sc-1eb5slv-0.iJjGCS'))
+        return data.map(td => td.innerText)
+    });
+
+    // Obtener precios
+
+    this.prices = await page.evaluate(() => {
+        const data = Array.from(document.querySelectorAll('.cmc-table tr td .price___3rj7O '))
+        return data.map(td => td.innerText)
+    });
+
+    // Obtener porcentajes semanales
+
+    this.weekPriceChange = await page.evaluate(() => {
+        const data = Array.from(document.querySelectorAll('.cmc-table tr td .sc-1v2ivon-0.jvNdfB'))
+        return data.map(td => td.innerText)
+    })
+
+    // Obtener Cap. Mercado
+
+    this.marketCap = await page.evaluate(() => {
+        const data = Array.from(document.querySelectorAll('.cmc-table tr td .sc-1eb5slv-0.kDEzev'))
+        return data.map(td => td.innerText)
+    })
+
+
+
+    for (i = 0; i <= this.cryptoName.length - 1; i++) {
+        cryptos.push({ name: this.cryptoName[i], price: this.prices[i], priceChanged: this.weekPriceChange[i], marketCap: this.marketCap[i] });
+    }
+
+    console.log(cryptos);
+    console.log(cryptos.length)
+
+    await browser.close();
+
+})();
+
+async function scrollDown(page) {
+
+
+        const distance = 100;
+        var totalHeight = 0;
+        const delay = 100;
+
+
+
+        while (totalHeight <= await page.evaluate(() => document.scrollingElement.scrollTop)) {
+
+            await page.evaluate((delay) => { document.scrollingElement.scrollBy(0, delay); }, distance);
+            totalHeight += distance;
+            await page.waitForTimeout(delay);
         }
 
-        console.log(cryptos);
-
-    });
+}
