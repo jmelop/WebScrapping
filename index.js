c5df15006a04481711755c36c85c8b6d03f9f201
Raúl Rodríguez <raul.rod16124@gmail.com>
2021-05-17 15:15:34 +0100

    Update info of webscrapping

--- index.js
+++ index.js
@@ -1,22 +1,61 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const url = "https://www.coingecko.com/es";
+const url = 'https://es.cointelegraph.com/category/market-analysis';
 
-// Functions
-function getCoin(){
+const news = [];
 
-    return axios.get(url)
-    .then( rest => {
+// Function to get the last 15 news
+async function getInfoCoins(){
 
-        const $ = cheerio.load( rest.data );
+    return axios.get( url ).then( rest => rest.data )
+                .then( infoCoins => {
+            
+                    const $ = cheerio.load(infoCoins);
+                    
+                    // --- General var
+                    var newsTitle = [];
+                    var newsDate = [];
+                    var newsAuthor = [];
+                    // var newsText = [];
+                    var newsViews = [];
 
-        const names = $('td.coin-name div div.center a:first-of-type');
-        
+                    // --- Get info section
+                    var title = $( 'li.posts-listing__item article.post-card-inline div.post-card-inline__content div.post-card-inline__header a span' ).text().split( '  ' );
+                    var date = $( 'li.posts-listing__item article.post-card-inline div.post-card-inline__content div.post-card-inline__meta time[datetime]' ).text().split( '  ' );
+                    var author = $( 'li.posts-listing__item article.post-card-inline div.post-card-inline__content div.post-card-inline__meta p.post-card-inline__author a' ).text().split( '  ' );
+                    // var text = $( 'li.posts-listing__item article.post-card-inline div.post-card-inline__content p.post-card-inline__text').text().split();
+                    var views = $( 'li.posts-listing__item article.post-card-inline div.post-card-inline__content div.post-card-inline__stats span span:last-of-type' ).text().split( '  ' );
 
-    });
+
+                    for( let indice = 0 ; indice < 11 ; indice ++ ){
+
+                        newsTitle.push( title[indice] );
+                        newsDate.push( date[indice] );
+                        newsAuthor.push( author[indice] );
+                        // newsText.push( text[indice] );
+                        newsViews.push( views[indice] );
+
+                        news.push({ Tittle : title[indice], Date : date[indice], Author : author[indice], Views : views[indice]});
+
+                    }
+                    
+
+                });
 
 
 }
 
-getCoin();
+console.log( 'Cargando noticias...' );
+
+setTimeout( () => {
+
+    console.log(news);
+
+    console.log( 'Acceso a las noticias: ' + url );
+
+}, 2000 );
+
+
+
+getInfoCoins();
